6d8a51eaabd6b70ef62271f1e36a98f376e8c471
SThor <paulgivel@gmail.com>
2018-06-15 00:17:22 +0200

    selection et affichage de rule

--- js/scripts.js
+++ js/scripts.js
@@ -4,86 +4,137 @@ var cellBorders = Object.freeze({
   RIGHT:3,
   LEFT:4
 });
-var ca = {canvas:null,ctx:null,cellWidth:10};
 
-ca.drawBoard = function(){
-  ca.cellWidth = parseInt(document.getElementById("input_cell").value);
-  // Box width
-  var bw = ca.canvas.width - (ca.canvas.width%ca.cellWidth) +1;
-  // Box height
-  var bh = ca.canvas.height- (ca.canvas.height%ca.cellWidth) +1;
+var grid = {
+  canvas:null,
+  ctx:null,
+  cellWidth:10,
 
+  drawBoard : function(){
+    grid.cellWidth = parseInt(document.getElementById("input_cell").value);
+    // Box width
+    var bw = grid.canvas.width - (grid.canvas.width%grid.cellWidth) +1;
+    // Box height
+    var bh = grid.canvas.height- (grid.canvas.height%grid.cellWidth) +1;
   
-  ca.ctx.beginPath();
+    
+    grid.ctx.beginPath();
+    
+    var x;
+    for (x = 0; x <= bw; x += grid.cellWidth) {
+        grid.ctx.moveTo(0.5 + x, 0);
+        grid.ctx.lineTo(0.5 + x, bh);
+    }
+    
+    
+    for (x = 0; x <= bh; x += grid.cellWidth) {
+        grid.ctx.moveTo(0, 0.5 + x);
+        grid.ctx.lineTo(bw, 0.5 + x);
+    }
+    
+    grid.ctx.strokeStyle = "black";
+    grid.ctx.stroke();
+  },
   
-  var x;
-  for (x = 0; x <= bw; x += ca.cellWidth) {
-      ca.ctx.moveTo(0.5 + x, 0);
-      ca.ctx.lineTo(0.5 + x, bh);
+  resizeCanvas : function() {
+    grid.canvas.width = window.innerWidth;
+    grid.canvas.height = window.innerHeight - grid.canvas.offsetTop;
+  
+    /**
+     * Your drawings need to be inside this function otherwise they will be reset when
+     * you resize the browser window and the canvas goes will be cleared.
+     */
+    grid.drawStuff();
+  },
+  
+  fillCell : function(cell){
+    pixel = grid.gridToPixel(cell,cellBorders.TOP,cellBorders.LEFT);
+    grid.ctx.fillRect(pixel.x,pixel.y,grid.cellWidth,grid.cellWidth);
+  },
+  
+  pixelToGrid : function(pixelPoint){
+    return {x:Math.floor((pixelPoint.x/grid.cellWidth)),y:Math.floor(pixelPoint.y/grid.cellWidth)};
+  },
+  
+  gridToPixel : function(cell,hBorder,vBorder){
+    var pixel = {x:cell.x*grid.cellWidth,y:cell.y*grid.cellWidth};
+    if(hBorder === cellBorders.BOTTOM){
+      pixel.x+=grid.cellWidth;
+    }
+    if(vBorder === cellBorders.RIGHT){
+      pixel.y+=grid.cellWidth;
+    }
+    return pixel;
+  },
+  
+  drawStuff : function() {
+    grid.ctx.clearRect(0, 0, grid.canvas.width, grid.canvas.height);
+    grid.drawBoard();
+    grid.ctx.fillRect(grid.canvas.width/2,10,1,1);
+    grid.fillCell({x:3,y:3})
   }
-  
-  
-  for (x = 0; x <= bh; x += ca.cellWidth) {
-      ca.ctx.moveTo(0, 0.5 + x);
-      ca.ctx.lineTo(bw, 0.5 + x);
-  }
-  
-  ca.ctx.strokeStyle = "black";
-  ca.ctx.stroke();
 };
 
-ca.init = function(){
-  ca.canvas = document.getElementById('canvas');
-  ca.ctx = ca.canvas.getContext('2d');
-  ca.canvas.addEventListener('mousedown',function(){
-    let point = {x:event.clientX-ca.canvas.offsetLeft,y:event.clientY-ca.canvas.offsetTop};
-    ca.fillCell(ca.pixelToGrid(point));
+var ruleDesigner = {
+  cellWidth:15,
+  canvas:null,
+  ctx:null,
+  drawRule : function(){
+    ruleDesigner.ctx.clearRect(0,0,ruleDesigner.canvas.width,ruleDesigner.canvas.height);
+    
+    var ruleBitWidth = Math.floor((ruleDesigner.canvas.width/8));
+    var vOffset = Math.floor((ruleDesigner.canvas.height - 2*ruleDesigner.cellWidth)/3);
+    var hOffsetTOP = Math.floor((ruleBitWidth-3*ruleDesigner.cellWidth)/2);
+    var hOffsetBOTTOM = Math.floor((ruleBitWidth-ruleDesigner.cellWidth)/2);
+    var ruleString = parseInt(document.getElementById('input_rule').value).toString(2).padStart(8,'0');
+    console.log('rule',ruleString);
+    
+    var pattern, char, i, j;
+    
+    for(i=0;i<8;i++){
+      ruleDesigner.ctx.strokeRect(i*ruleBitWidth+0.5,0.5,ruleBitWidth,ruleDesigner.canvas.height-0.5);
+      pattern = (7-i).toString(2).padStart(3,'0');
+      for(j=0;j<3;j++){
+        char = pattern.charAt(j);
+        if(char==='1'){
+          ruleDesigner.ctx.fillRect(i*ruleBitWidth+hOffsetTOP+j*ruleDesigner.cellWidth+0.5,vOffset+0.5,ruleDesigner.cellWidth,ruleDesigner.cellWidth);
+        }else{
+          ruleDesigner.ctx.strokeRect(i*ruleBitWidth+hOffsetTOP+j*ruleDesigner.cellWidth+0.5,vOffset+0.5,ruleDesigner.cellWidth,ruleDesigner.cellWidth);
+        }
+      }
+      char = ruleString.charAt(i);
+      if(char==='1'){
+        ruleDesigner.ctx.fillRect(i*ruleBitWidth+hOffsetBOTTOM+0.5,2*vOffset+ruleDesigner.cellWidth+0.5,ruleDesigner.cellWidth,ruleDesigner.cellWidth);
+      }else{
+        ruleDesigner.ctx.strokeRect(i*ruleBitWidth+hOffsetBOTTOM+0.5,2*vOffset+ruleDesigner.cellWidth+0.5,ruleDesigner.cellWidth,ruleDesigner.cellWidth);
+      }
+    }
+    
+  }
+};
+  
+init = function(){
+  ruleDesigner.canvas = document.getElementById('canvas_rule');
+  ruleDesigner.canvas.height = 50;
+  ruleDesigner.canvas.width = ruleDesigner.canvas.parentNode.width;
+  ruleDesigner.canvas.width = 500;
+  console.log("ruleDesigner.canvas.width",ruleDesigner.canvas.width)
+  ruleDesigner.ctx = ruleDesigner.canvas.getContext("2d");
+  document.getElementById('input_rule').addEventListener('input',ruleDesigner.drawRule)
+  
+  grid.canvas = document.getElementById('canvas');
+  grid.ctx = grid.canvas.getContext('2d');
+  grid.canvas.addEventListener('mousedown',function(){
+    var point = {x:event.clientX-grid.canvas.offsetLeft,y:event.clientY-grid.canvas.offsetTop};
+    grid.fillCell(grid.pixelToGrid(point));
     
   });
   
-  document.getElementById("input_cell").addEventListener('input',ca.drawStuff);
+  document.getElementById("input_cell").addEventListener('input',grid.drawStuff);
 
   // resize the canvas to fill browser window dynamically
-  window.addEventListener('resize', ca.resizeCanvas, false);
-  ca.resizeCanvas();
+  window.addEventListener('resize', grid.resizeCanvas, false);
+  grid.resizeCanvas();
 };
 
-ca.resizeCanvas = function() {
-  ca.canvas.width = window.innerWidth;
-  ca.canvas.height = window.innerHeight - ca.canvas.offsetTop;
-
-  /**
-   * Your drawings need to be inside this function otherwise they will be reset when
-   * you resize the browser window and the canvas goes will be cleared.
-   */
-  ca.drawStuff();
-};
-
-ca.fillCell = function(cell){
-  pixel = ca.gridToPixel(cell,cellBorders.TOP,cellBorders.LEFT);
-  ca.ctx.fillRect(pixel.x,pixel.y,ca.cellWidth,ca.cellWidth);
-};
-
-ca.pixelToGrid = function(pixelPoint){
-  return {x:Math.floor((pixelPoint.x/ca.cellWidth)),y:Math.floor(pixelPoint.y/ca.cellWidth)};
-};
-
-ca.gridToPixel = function(cell,hBorder,vBorder){
-  var pixel = {x:cell.x*ca.cellWidth,y:cell.y*ca.cellWidth};
-  if(hBorder === cellBorders.BOTTOM){
-    pixel.x+=ca.cellWidth;
-  }
-  if(vBorder === cellBorders.RIGHT){
-    pixel.y+=ca.cellWidth;
-  }
-  return pixel;
-};
-
-ca.drawStuff = function() {
-  ca.ctx.clearRect(0, 0, ca.canvas.width, ca.canvas.height);
-  ca.drawBoard();
-  ca.ctx.fillRect(ca.canvas.width/2,10,1,1);
-  ca.fillCell({x:3,y:3})
-};
-
-window.addEventListener("load", ca.init());
+window.addEventListener("load", init());
