c75501c6d923fa80b4e6e8793830ef7b7bb50248
SThor <paulgivel@gmail.com>
2018-07-28 00:22:11 +0200

    first commit

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,54 @@
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+function hexToRGB(hex, alpha) {
+    var r = parseInt(hex.slice(1, 3), 16),
+        g = parseInt(hex.slice(3, 5), 16),
+        b = parseInt(hex.slice(5, 7), 16);
+
+    if (alpha) {
+        return "rgba(" + r + ", " + g + ", " + b + ", " + alpha + ")";
+    } else {
+        return "rgb(" + r + ", " + g + ", " + b + ")";
+    }
+}
+
 var canvas = {
   canvas:null,
   ctx:null,
-  color_bg:"white",
+  color_bg:"#F4E8C1", //"white"
   color_main:"black",
   padding:50,
   settings:{
-    cellWidth:2,
+    cellsInWidth : 10,
+    //cellWidth:50,
+    spacing:10,
     radius:100,
     modulo:3
   },
+  // palette:[
+  //   "#bbf67f",
+  //   "#a6df86",
+  //   "#91c88c",
+  //   "#7bb093",
+  //   "#669999",
+  //   "#e20245"
+  //   ],
+  // palette:[
+  //   "#6eb686",
+  //   "#9aac84",
+  //   "#b0a782",
+  //   "#d1a081",
+  //   "#e79b80",
+  //   ],
   palette:[
-    "#bbf67f",
-    "#a6df86",
-    "#91c88c",
-    "#7bb093",
-    "#669999"
+    "#ffff00",
+    "#ff007f",
+    "#00ffff",
+    "#fc961f"
     ],
   resizeCanvas : function() {
     //canvas.width = window.innerWidth;
@@ -66,38 +100,66 @@ var canvas = {
     var topCenter = {"x":canvas.canvas.width/2,"y":0};
     canvas.ctx.setTransform(1, 0, 0, 1, 0, 0);
     canvas.ctx.clearRect(0,0,canvas.canvas.width,canvas.canvas.height);
-    canvas.ctx.fillStyle = canvas.color_bg;
-    canvas.ctx.fillRect(0,0,canvas.canvas.width,canvas.canvas.height);
-    canvas.ctx.fillStyle = canvas.color_main;
-    for(var x=0;x<canvas.canvas.width;x+=canvas.settings.cellWidth){
-      for(var y=0;y<canvas.canvas.height;y+=canvas.settings.cellWidth){
-        point = {"x":x,"y":y};
-        canvas.ctx.fillStyle = "rgba(0,0,0,"+(((point.x ^ point.y) % canvas.settings.modulo )/(canvas.settings.modulo*2))+")";
-        if(canvas.getCondition(point)) canvas.fillCell(point);
-        
-        canvas.ctx.fillStyle = canvas.color_main;
-        if(canvas.getConditionBis(point)) canvas.fillCell(point);
+    canvas.drawBackground();
+    
+    var cellLength = ((canvas.canvas.width-4*canvas.padding-(canvas.settings.cellsInWidth-1)*canvas.settings.spacing)/canvas.settings.cellsInWidth);
+    for(i=0;i<canvas.settings.cellsInWidth;i++){
+      for(j=0;j<canvas.settings.cellsInWidth;j++){
+        canvas.ctx.setTransform(1,0,0,1,(i*(cellLength+canvas.settings.spacing))+0.5+2*canvas.padding,(j*(cellLength+canvas.settings.spacing))+0.5+2*canvas.padding);
+        var hex = canvas.palette[getRandomInt(0,canvas.palette.length-1)];
+        var hex2 = canvas.palette[getRandomInt(0,canvas.palette.length-1)];
+        var gradient = canvas.ctx.createLinearGradient(0,0,getRandomInt(0,cellLength),cellLength);
+        gradient.addColorStop(0,hexToRGB(hex,0.2));
+        gradient.addColorStop(1,hexToRGB(hex2,0.2));
+        canvas.ctx.strokeStyle = gradient
+        canvas.drawCell(i,j,cellLength);
+        canvas.drawCell(i,j,cellLength);
+        canvas.drawCell(i,j,cellLength);
+        canvas.drawCell(i,j,cellLength);
+        canvas.drawCell(i,j,cellLength);
+        canvas.drawCell(i,j,cellLength);
       }
     }
+    // for(var x=0;x<canvas.canvas.width;x+=cellLength+canvas.settings.spacing){
+    //   for(var y=0;y<canvas.canvas.height;y+=cellLength+canvas.settings.spacing){
+    //     canvas.drawCell(x,y,cellLength);
+    //     canvas.drawCell(x,y,cellLength);
+    //   }
+    // }
+  },
+  
+  drawCell: function(i,j,cellLength){
+    var it, cp1x,cp1y,cp2x,cp2y,cp3x,cp3y;
+    canvas.ctx.moveTo(getRandomInt(0,cellLength),getRandomInt(0,cellLength));
+    canvas.ctx.beginPath();
+    // canvas.ctx.strokeStyle = 'rgba(0,' + Math.floor(255 - 42.5 * i) + ',' +
+    //                   Math.floor(255 - 42.5 * j) + ',0.2)';
+    for(it=0;it<5;it++){
+      cp1x = getRandomInt(0,cellLength);
+      cp2x = getRandomInt(0,cellLength);
+      cp3x = getRandomInt(0,cellLength);
+      cp1y = getRandomInt(0,cellLength);
+      cp2y = getRandomInt(0,cellLength);
+      cp3y = getRandomInt(0,cellLength);
+      canvas.ctx.bezierCurveTo(cp1x,cp1y,cp2x,cp2y,cp3x,cp3y);
+    }
+    canvas.ctx.stroke();
+  },
+  
+  drawBackground: function(){
+    canvas.ctx.fillStyle = "white";
+    canvas.ctx.fillRect(0,0,canvas.canvas.width,canvas.canvas.height);
+    var i;
+    // canvas.ctx.fillStyle = canvas.color_bg;
+    // for(i=0;i<10000;i++){
+    //   canvas.ctx.fillRect(getRandomInt(canvas.padding,canvas.canvas.width-canvas.padding),getRandomInt(canvas.padding,canvas.canvas.height-canvas.padding),1,1);
+    // }
   },
   
   distance: function(point1, point2){
     var a = point1.x - point2.x;
     var b = point1.y - point2.y;
     return Math.sqrt( a*a + b*b );
-  },
-  
-  getCondition: function(point){
-    var center = {"x":canvas.canvas.width/2,"y":canvas.canvas.height/2};
-    var bottomCenter = {"x":canvas.canvas.width/2,"y":canvas.canvas.height};
-    var topCenter = {"x":canvas.canvas.width/2,"y":0};
-    return (/*canvas.distance(point,center)<canvas.settings.radius|| */canvas.distance(point,bottomCenter)<canvas.settings.radius || canvas.distance(point,topCenter)<canvas.settings.radius);//&& (((point.x ^ point.y) % 3 ) %2 === 1);
-  },
-  
-  getConditionBis: function(point){
-    var center = {"x":canvas.canvas.width/2,"y":canvas.canvas.height/2};
-    var topCenter = {"x":canvas.canvas.width/2,"y":0};
-    return (canvas.distance(point,center)<canvas.settings.radius || canvas.distance(point,topCenter)<canvas.settings.radius) && (((point.x ^ point.y) % canvas.settings.modulo ) %2 === 1);
   },
   
   fillCell: function(point) {
